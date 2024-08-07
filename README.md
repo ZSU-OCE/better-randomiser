add1010919d4ab4e507e63f55c681599cfee5ce1
Martinez <logan.m269@gmail.com>
2024-08-07 20:52:06 +1000

    Stable v1.0

--- README.md
+++ README.md
@@ -8,35 +8,44 @@ The `BetterRandomiser` plugin provides an automated system for randomising teams
 - **Stop Randomisation**: Admins can stop the scheduled team randomisation if necessary.
 - **Intervalled Updates**: Periodically updates the team list every 5 minutes until the new game starts.
 - **Broadcasts**: Informs players about upcoming randomisations and cancellations via server broadcasts.
+- **Continuous Checking**: Checks and swaps players for 60 seconds after a new game starts to ensure they are in the correct teams. After 60 seconds, balances teams if needed.
 
 ## Admin Commands
 
 - **Start Randomisation**: Initiates the team randomisation process.
   - Default Command: `!randomise`
-  - **Usage**: Type the command in the in-game admin chat to start the process.
+  - **Usage**: Admin types the command in the in-game admin chat to start the process.
 
 - **Stop Randomisation**: Cancels the scheduled team randomisation.
   - Default Command: `!stoprandomise`
-  - **Usage**: Type the command in the in-game admin chat to stop the process.
+  - **Usage**: Admin types the command in the in-game admin chat to stop the process.
 
 ## Configuration Options
 
 The following options can be configured in the plugin's configuration file:
 
 - **command**: The command used to initiate team randomisation.
-  - **Default**: `!randomise`
+  - **Default**: `randomise`
   
 - **stopCommand**: The command used to stop the team randomisation.
-  - **Default**: `!stoprandomise`
+  - **Default**: `stoprandomise`
 
 - **startBroadcast**: The message broadcasted when the team randomisation is scheduled.
   - **Default**: `We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated.`
+  - **enableStartBroadcast**: Enable or disable the start broadcast.
+  - **Default**: `true`
 
 - **stopBroadcast**: The message broadcasted when the team randomisation is cancelled.
   - **Default**: `Team randomisation has been cancelled.`
+  - **enableStopBroadcast**: Enable or disable the stop broadcast.
+  - **Default**: `true`
 
-- **warnPlayerMessage**: The message sent to warn players before team randomisation.
-  - **Default**: `Initiating team randomise. You may be swapped.`
+- **intervalBroadcast**: The message broadcasted at intervals before the randomisation occurs.
+  - **Default**: `We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated.`
+  - **enableIntervalBroadcast**: Enable or disable the interval broadcast.
+  - **Default**: `true`
+  - **intervalTime**: The interval time in minutes for the interval broadcast.
+  - **Default**: `5`
 
 - **alreadyScheduledMessage**: The message sent to the player if team randomisation is already scheduled.
   - **Default**: `Team randomisation is already scheduled.`
@@ -44,6 +53,11 @@ The following options can be configured in the plugin's configuration file:
 - **notScheduledMessage**: The message sent to the player if team randomisation has not been scheduled yet.
   - **Default**: `Team randomisation has not been scheduled yet.`
 
+- **checkInterval**: The interval in seconds for checking and swapping players after the new game starts.
+  - **Default**: `5`
+
+- **totalCheckTime**: The total time in seconds to continue checking and swapping players after the new game starts.
+  - **Default**: `60`
 
 ## Example Configuration
 
@@ -54,8 +68,14 @@ The following options can be configured in the plugin's configuration file:
   "command": "randomise",
   "stopCommand": "stoprandomise",
   "startBroadcast": "We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated.",
+  "enableStartBroadcast": true,
   "stopBroadcast": "Team randomisation has been cancelled.",
-  "warnPlayerMessage": "Initiating team randomise. You may be swapped.",
+  "enableStopBroadcast": true,
+  "intervalBroadcast": "We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated.",
+  "enableIntervalBroadcast": true,
+  "intervalTime": 5,
   "alreadyScheduledMessage": "Team randomisation is already scheduled.",
-  "notScheduledMessage": "Team randomisation has not been scheduled yet."
+  "notScheduledMessage": "Team randomisation has not been scheduled yet.",
+  "checkInterval": 5,
+  "totalCheckTime": 60
 }
--- plugins/better-randomiser.js
+++ plugins/better-randomiser.js
@@ -3,7 +3,7 @@ import BasePlugin from './base-plugin.js';
 export default class BetterRandomiser extends BasePlugin {
   static get description() {
     return (
-      "The <code>TeamRandomiser</code> can be used to randomise teams. It's great for destroying clan stacks or for " +
+      "The <code>BetterRandomiser</code> can be used to randomise teams. It's great for destroying clan stacks or for " +
       'social events. It can be run by typing, by default, <code>!randomise</code> into in-game admin chat'
     );
   }
@@ -29,15 +29,35 @@ export default class BetterRandomiser extends BasePlugin {
         description: 'The message broadcasted when the team randomisation is scheduled.',
         default: "We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated."
       },
+      enableStartBroadcast: {
+        required: false,
+        description: 'Enable or disable the start broadcast.',
+        default: true
+      },
       stopBroadcast: {
         required: false,
         description: 'The message broadcasted when the team randomisation is cancelled.',
         default: "Team randomisation has been cancelled."
       },
-      warnPlayerMessage: {
+      enableStopBroadcast: {
         required: false,
-        description: 'The message sent to warn players before team randomisation.',
-        default: "Initiating team randomise. You may be swapped."
+        description: 'Enable or disable the stop broadcast.',
+        default: true
+      },
+      intervalBroadcast: {
+        required: false,
+        description: 'The message broadcasted at intervals before the randomisation occurs.',
+        default: "We will be shuffling teams at the start of next game. We will attempt to keep you together with your squad, but this isn't guaranteed. This system is automated."
+      },
+      enableIntervalBroadcast: {
+        required: false,
+        description: 'Enable or disable the interval broadcast.',
+        default: true
+      },
+      intervalTime: {
+        required: false,
+        description: 'The interval time in minutes for the interval broadcast.',
+        default: 5
       },
       alreadyScheduledMessage: {
         required: false,
@@ -48,6 +68,16 @@ export default class BetterRandomiser extends BasePlugin {
         required: false,
         description: 'The message sent to the player if team randomisation has not been scheduled yet.',
         default: "Team randomisation has not been scheduled yet."
+      },
+      checkInterval: {
+        required: false,
+        description: 'The interval in seconds for checking and swapping players after the new game starts.',
+        default: 5
+      },
+      totalCheckTime: {
+        required: false,
+        description: 'The total time in seconds to continue checking and swapping players after the new game starts.',
+        default: 60
       }
     };
   }
@@ -60,6 +90,8 @@ export default class BetterRandomiser extends BasePlugin {
     this.updateSquadList = this.updateSquadList.bind(this);
     this.savedTeams = null;
     this.updateInterval = null;
+    this.intervalBroadcastInterval = null;
+    this.checkInterval = null;
     this.isScheduled = false;
   }
 
@@ -75,6 +107,12 @@ export default class BetterRandomiser extends BasePlugin {
     this.server.removeEventListener('NEW_GAME', this.onNewGame);
     if (this.updateInterval) {
       clearInterval(this.updateInterval);
+    }
+    if (this.intervalBroadcastInterval) {
+      clearInterval(this.intervalBroadcastInterval);
+    }
+    if (this.checkInterval) {
+      clearInterval(this.checkInterval);
     }
   }
 
@@ -163,14 +201,23 @@ export default class BetterRandomiser extends BasePlugin {
     if (info.chat !== 'ChatAdmin') return;
 
     if (this.isScheduled) {
-      await this.server.rcon.warn(info.steamID, this.options.alreadyScheduledMessage);
+      await this.server.rcon.warn(info.eosID, this.options.alreadyScheduledMessage);
       return;
     }
 
     await this.updateSquadList();
     this.updateInterval = setInterval(this.updateSquadList, 300000); // Update every 5 minutes
 
-    this.broadcast(this.options.startBroadcast);
+    if (this.options.enableStartBroadcast) {
+      this.broadcast(this.options.startBroadcast);
+    }
+
+    if (this.options.enableIntervalBroadcast) {
+      this.intervalBroadcastInterval = setInterval(
+        () => this.broadcast(this.options.intervalBroadcast),
+        this.options.intervalTime * 60000
+      ); // Interval broadcast every X minutes
+    }
 
     this.verbose(1, `Teams have been saved and will be updated every 5 minutes until the new game starts.`);
     this.isScheduled = true;
@@ -180,16 +227,22 @@ export default class BetterRandomiser extends BasePlugin {
     if (info.chat !== 'ChatAdmin') return;
 
     if (!this.isScheduled) {
-      await this.server.rcon.warn(info.steamID, this.options.notScheduledMessage);
+      await this.server.rcon.warn(info.eosID, this.options.notScheduledMessage);
       return;
     }
 
     clearInterval(this.updateInterval);
+    clearInterval(this.intervalBroadcastInterval);
+    clearInterval(this.checkInterval);
     this.updateInterval = null;
+    this.intervalBroadcastInterval = null;
+    this.checkInterval = null;
     this.savedTeams = null;
     this.isScheduled = false;
 
-    this.broadcast(this.options.stopBroadcast);
+    if (this.options.enableStopBroadcast) {
+      this.broadcast(this.options.stopBroadcast);
+    }
 
     this.verbose(1, `Team randomisation process has been cancelled.`);
   }
@@ -198,152 +251,81 @@ export default class BetterRandomiser extends BasePlugin {
     const players = this.server.players.slice(0);
     await this.logCurrentTeams(players);
 
-    const teams = this.organizeTeams(players);
-    const newTeams = { 1: [], 2: [] };
-    let team1Count = 0;
-    let team2Count = 0;
-
-    // Shuffle squads for randomness
-    const allSquads = teams[1].squads.concat(teams[2].squads);
-    for (let i = allSquads.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [allSquads[i], allSquads[j]] = [allSquads[j], allSquads[i]];
-    }
-
-    // Move squads to new teams
-    for (const squad of allSquads) {
-      if (squad.squadID === '0') continue; // Skip unassigned
-      const squadSize = squad.players.length;
-      if (team1Count <= team2Count) {
-        newTeams[1].push(...squad.players);
-        team1Count += squadSize;
-      } else {
-        newTeams[2].push(...squad.players);
-        team2Count += squadSize;
-      }
-    }
-
-    // Use unassigned players to balance teams
-    const unassignedPlayers = teams[1].squads[0].players.concat(teams[2].squads[0].players);
-    for (const player of unassignedPlayers) {
-      if (team1Count <= team2Count) {
-        newTeams[1].push(player);
-        team1Count++;
-      } else {
-        newTeams[2].push(player);
-        team2Count++;
-      }
-    }
-
-    // Final balance check and swap smaller squads if needed
-    while (Math.abs(team1Count - team2Count) > 2) {
-      const largerTeam = team1Count > team2Count ? 1 : 2;
-      const smallerTeam = largerTeam === 1 ? 2 : 1;
-
-      const smallestSquadIndex = newTeams[largerTeam].findIndex(player =>
-        newTeams[largerTeam].filter(p => p.squadID === player.squadID).length === 1
-      );
-
-      if (smallestSquadIndex === -1) break;
-
-      const smallestSquad = newTeams[largerTeam].splice(smallestSquadIndex, 1);
-      newTeams[smallerTeam].push(...smallestSquad);
-
-      team1Count = newTeams[1].length;
-      team2Count = newTeams[2].length;
-    }
-
-    // Final balance check and split the highest squad if needed
-    while (Math.abs(team1Count - team2Count) > 2) {
-      const largerTeam = team1Count > team2Count ? 1 : 2;
-      const smallerTeam = largerTeam === 1 ? 2 : 1;
-
-      const largestSquad = newTeams[largerTeam].reduce((prev, curr) =>
-        prev.squadID > curr.squadID ? prev : curr
-      );
-
-      const splitIndex = Math.ceil(largestSquad.players.length / 2);
-      newTeams[smallerTeam].push(...largestSquad.players.splice(0, splitIndex));
-      newTeams[largerTeam] = newTeams[largerTeam].filter(
-        player => player.squadID !== largestSquad.squadID
-      );
-      newTeams[largerTeam].push(...largestSquad.players);
-
-      team1Count = newTeams[1].length;
-      team2Count = newTeams[2].length;
-    }
-
-    this.savedTeams = newTeams;
+    this.savedTeams = this.organizeTeams(players);
 
     this.verbose(1, `Updated squad list has been saved.`);
   }
 
-  async warnPlayers() {
-    for (const player of this.server.players) {
-      if (!player.steamID) {
-        this.verbose(1, `Skipping player ${player.name} due to missing steamID`);
-        continue;
-      }
-
-      try {
-        await this.server.rcon.warn(player.steamID, this.options.warnPlayerMessage);
-      } catch (error) {
-        this.verbose(1, `Failed to warn player ${player.name}: ${error.message}`);
-      }
-    }
-  }
-
   async onNewGame() {
     if (this.savedTeams) {
-      this.verbose(1, `Executing saved team randomisation in 20 seconds...`);
+      this.verbose(1, `Executing saved team randomisation immediately and checking for the next 60 seconds...`);
       clearInterval(this.updateInterval); // Stop updating the squad list
+      clearInterval(this.intervalBroadcastInterval); // Stop interval broadcasts
 
-      setTimeout(async () => {
-        await this.warnPlayers();
-      }, 15000); // Warn players 15 seconds after the new game starts
-
-      setTimeout(async () => {
+      const switchTeams = async () => {
         const players = this.server.players.slice(0);
-        const newUnassignedPlayers = players.filter(
-          player => !this.savedTeams[1].includes(player) && !this.savedTeams[2].includes(player)
-        );
-
-        // Add new unassigned players to the saved teams
-        for (const player of newUnassignedPlayers) {
-          if (this.savedTeams[1].length <= this.savedTeams[2].length) {
-            this.savedTeams[1].push(player);
-          } else {
-            this.savedTeams[2].push(player);
+        for (const player of this.savedTeams[1]) {
+          const serverPlayer = players.find(p => p.eosID === player.eosID);
+          if (serverPlayer && serverPlayer.teamID !== '1') {
+            await this.server.rcon.switchTeam(player.eosID);
           }
         }
+        for (const player of this.savedTeams[2]) {
+          const serverPlayer = players.find(p => p.eosID === player.eosID);
+          if (serverPlayer && serverPlayer.teamID !== '2') {
+            await this.server.rcon.switchTeam(player.eosID);
+          }
+        }
+      };
 
-        // Final check for balance
-        const team1Count = this.savedTeams[1].length;
-        const team2Count = this.savedTeams[2].length;
-        const difference = Math.abs(team1Count - team2Count);
+      // Switch teams immediately
+      await switchTeams();
 
-        if (difference >= 4) {
-          const largerTeam = team1Count > team2Count ? 1 : 2;
-          const smallerTeam = largerTeam === 1 ? 2 : 1;
+      // Check and switch teams every X seconds for 60 seconds
+      const checkIntervalDuration = this.options.checkInterval * 1000;
+      const totalCheckDuration = this.options.totalCheckTime * 1000;
+      let elapsedTime = 0;
 
-          const unassignedPlayersFromLargerTeam = this.savedTeams[largerTeam].filter(player => player.squadID === '0');
+      this.checkInterval = setInterval(async () => {
+        if (elapsedTime >= totalCheckDuration) {
+          clearInterval(this.checkInterval);
+          this.checkInterval = null;
 
-          for (let i = 0; i < Math.floor(difference / 2); i++) {
-            if (unassignedPlayersFromLargerTeam.length > 0) {
-              const player = unassignedPlayersFromLargerTeam.pop();
-              this.savedTeams[smallerTeam].push(player);
-              this.savedTeams[largerTeam] = this.savedTeams[largerTeam].filter(p => p !== player);
-            } else {
-              break;
+          // Final balance check and adjustments if needed
+          const team1Count = this.savedTeams[1].length;
+          const team2Count = this.savedTeams[2].length;
+          const difference = Math.abs(team1Count - team2Count);
+
+          if (difference >= 4) {
+            const largerTeam = team1Count > team2Count ? 1 : 2;
+            const smallerTeam = largerTeam === 1 ? 2 : 1;
+
+            const unassignedPlayersFromLargerTeam = this.savedTeams[largerTeam].filter(player => player.squadID === '0');
+
+            for (let i = 0; i < Math.floor(difference / 2); i++) {
+              if (unassignedPlayersFromLargerTeam.length > 0) {
+                const player = unassignedPlayersFromLargerTeam.pop();
+                this.savedTeams[smallerTeam].push(player);
+                this.savedTeams[largerTeam] = this.savedTeams[largerTeam].filter(p => p !== player);
+                await this.server.rcon.switchTeam(player.eosID); // Swap the unassigned player
+              } else {
+                break;
+              }
             }
           }
+
+          this.verbose(1, `Adjusted team 1: ${this.savedTeams[1].map(player => player.name).join(', ')}`);
+          this.verbose(1, `Adjusted team 2: ${this.savedTeams[2].map(player => player.name).join(', ')}`);
+
+          return;
         }
 
-        this.verbose(1, `New randomised team 1: ${this.savedTeams[1].map(player => player.name).join(', ')}`);
-        this.verbose(1, `New randomised team 2: ${this.savedTeams[2].map(player => player.name).join(', ')}`);
-        this.savedTeams = null;
-        this.isScheduled = false;
-      }, 20000); // 20 seconds delay
+        await switchTeams();
+        elapsedTime += checkIntervalDuration;
+      }, checkIntervalDuration);
+
+      this.savedTeams = null;
+      this.isScheduled = false;
     }
   }
 }
